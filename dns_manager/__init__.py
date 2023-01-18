e6258a07ad8b737ce707db657c338277783db414
Ubuntu <33207042+szafranski-pawel@users.noreply.github.com>
2023-01-18 22:55:02 +0100

    change database address to env variable

--- dns_manager/__init__.py
+++ dns_manager/__init__.py
@@ -1,5 +1,6 @@
 import dataclasses
 import datetime
+import os
 from json import JSONEncoder
 import logging
 from flask import Flask, has_request_context, request
@@ -42,7 +43,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_ADDRESS']
     app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
     app.config['SESSION_COOKIE_HTTPONLY'] = True
     app.config['FLASK_PYDANTIC_VALIDATION_ERROR_RAISE'] = False
